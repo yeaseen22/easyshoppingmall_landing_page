17fa93a31646f5532809b2d4a54f22525f24ff8a
Programmer Rakibul <rakibul00206@gmail.com>
2026-07-05 22:59:34 +0600

    bg image added for sale countdown

--- src/features/home/components/sale-countdown/sale-countdown.jsx
+++ src/features/home/components/sale-countdown/sale-countdown.jsx
@@ -3,14 +3,13 @@
 import Container from "@/components/ui/container";
 import Section from "@/components/ui/section";
 import { getSaleCountDown } from "@/features/home/actions/sale-countdown";
+import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 const SaleCountDown = () => {
   const [targetDate, setTargetDate] = useState(null);
-  const [title, setTitle] = useState("Limited Time Offer - Up to 40% Off!");
-  const [description, setDescription] = useState(
-    "Don't miss out on this exclusive offer. Hurry, the clock is ticking!",
-  );
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     async function fetchConfig() {
@@ -52,50 +51,67 @@ const SaleCountDown = () => {
   }, [targetDate, calculateTimeLeft]);
 
   const timeBlock = (value, label) => (
-    <div className="flex flex-col items-center">
-      <div className="bg-[#2C2B30] p-2.5 sm:p-4 rounded-lg shadow-md text-primary-color text-xl font-mono">
+    <div className="bg-[#2C2B30] size-12.5 sm:size-18 p-1.5 rounded-lg shadow-md flex justify-center flex-col items-center">
+      <span className="text-primary-color text-base sm:text-xl">
         {(value ?? 0).toString().padStart(2, "0")}
-      </div>
-      <div className="text-[10px] sm:text-xs text-[#AAAAAA] mt-1">{label}</div>
+      </span>
+      <span className="text-[8px] sm:text-xs text-[#AAAAAA] mt-1">
+        {label}
+      </span>
     </div>
   );
 
   return (
-    <Section className="w-full bg-[#1C1A18] text-center min-h-auto py-10 md:py-16">
-      <Container className="max-w-2xl rounded-xl">
-        <p className="inline-block px-3 py-1 rounded-full text-xs sm:text-sm mb-3 bg-[#622c1c] text-primary-color">
-          {timeLeft?.expired ? "🎉 Sale Ended" : "🔥 Limited Time Only"}
-        </p>
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2 text-accent-content">
-          {title}
-        </h1>
-        <p className="mb-8 text-[#B0B0B0] text-sm sm:text-base md:text-lg">
-          {description}
-        </p>
+    <Section className="text-center min-h-auto py-10 md:py-16 relative">
+      {/* Background stars */}
+      <div className="absolute w-full h-full top-0 left-0">
+        <Image
+          src={"/glowing_stars.svg"}
+          alt="Glowing Stars"
+          width={1200}
+          height={600}
+          sizes="100vw"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-        {timeLeft && !timeLeft.expired && (
-          <div className="grid grid-cols-7 place-items-center gap-1 mb-8 max-w-xs mx-auto">
-            {timeBlock(timeLeft.days, "DAYS")}
-            <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
-              :
-            </span>
-            {timeBlock(timeLeft.hours, "HOURS")}
-            <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
-              :
-            </span>
-            {timeBlock(timeLeft.minutes, "MINUTES")}
-            <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
-              :
-            </span>
-            {timeBlock(timeLeft.seconds, "SECONDS")}
-          </div>
-        )}
+      {/* Content */}
+      <Container className="max-w-2xl z-1">
+        <div className=" rounded-xl bg-white/7 backdrop-blur-md shadow-3xl shadow-primary-color/40 p-5">
+          <p className="inline-block px-3 py-1 rounded-full text-xs sm:text-sm mb-3 bg-[#622c1c] text-primary-color">
+            {timeLeft?.expired ? "🎉 Sale Ended" : "🔥 Limited Time Only"}
+          </p>
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2 text-accent-content">
+            {title}
+          </h1>
+          <p className="mb-8 text-[#B0B0B0] text-sm sm:text-base md:text-lg">
+            {description}
+          </p>
 
-        {timeLeft?.expired && (
-          <div className="text-xl text-gray-500 font-bold py-6">
-            This sale has ended. Stay tuned for new offers!
-          </div>
-        )}
+          {timeLeft && !timeLeft.expired && (
+            <div className="grid grid-cols-7 place-items-center gap-1 mb-8 max-w-xs mx-auto">
+              {timeBlock(timeLeft.days, "DAYS")}
+              <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
+                :
+              </span>
+              {timeBlock(timeLeft.hours, "HOURS")}
+              <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
+                :
+              </span>
+              {timeBlock(timeLeft.minutes, "MINUTES")}
+              <span className="text-xl font-bold text-[#AAAAAA] flex items-center">
+                :
+              </span>
+              {timeBlock(timeLeft.seconds, "SECONDS")}
+            </div>
+          )}
+
+          {timeLeft?.expired && (
+            <div className="text-xl text-gray-500 font-bold py-6">
+              This sale has ended. Stay tuned for new offers!
+            </div>
+          )}
+        </div>
       </Container>
     </Section>
   );
